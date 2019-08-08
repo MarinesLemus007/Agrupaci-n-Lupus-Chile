613186940c8d37677bd795e5e459e3290f1bac03
Cloiw <sircli@hotmail.com>
2019-08-08 18:22:07 -0400

    detalles css de navbar

--- src/views/ContactView.js
+++ src/views/ContactView.js
@@ -3,7 +3,7 @@ import './styles/contactView.css';
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { Row, Col, Container } from 'react-bootstrap';
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Breadcrumb from '../components/BreadCrumb';
 import pin from "../img/iconosPng/pin_purple.png";
 import whatsapp from "../img/iconosPng/whatsapp_purple.png";
 import email from "../img/iconosPng/email_purple.png";
@@ -15,14 +15,9 @@ class ContactView extends React.Component{
             <>
                 
                 <NavBar/>
-                <Breadcrumb>
-                    <Breadcrumb.Item href="/">Inicio</Breadcrumb.Item>
-                    <Breadcrumb.Item active href='/Contacto'>
-                    Contacto
-                    </Breadcrumb.Item>
-                </Breadcrumb>
             <Container>
-                <section className="definition-0">
+                <Breadcrumb name="Contacto" />
+                <section className="section-contact-title">
                     <Row>
                         <Col>
                             <h1 className="definition-title">Contáctanos</h1>
@@ -30,6 +25,7 @@ class ContactView extends React.Component{
                         </Col>
                     </Row>
                 </section>
+                <section className="section-contact">
                 <Row>
                 <Col sm={6}>
                         <Row>
@@ -58,12 +54,14 @@ class ContactView extends React.Component{
                         </Row>
                     </Col>
                     <Col sm={6}>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3324.0730098324657!2d-70.58359698479737!3d-33.577453580737746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662d725b4a98067%3A0x62a60b0a60a05f60!2sHospital+Dr.+S%C3%B3tero+del+R%C3%ADo!5e0!3m2!1ses!2scl!4v1565088790602!5m2!1ses!2scl" allowfullscreen title="Mapa"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3324.0730098324657!2d-70.58359698479737!3d-33.577453580737746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662d725b4a98067%3A0x62a60b0a60a05f60!2sHospital+Dr.+S%C3%B3tero+del+R%C3%ADo!5e0!3m2!1ses!2scl!4v1565088790602!5m2!1ses!2scl" allowFullScreen title="Mapa"></iframe>
 
                     </Col>
                     </Row>
+                    </section>
+                    <Submit/>
             </Container>
-            <Submit/>
+            
                 <Footer/>
             </>
         )
